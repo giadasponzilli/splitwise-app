61a293a137e275916e965644d2e6d5bef5d8e005
Giada Sponzilli <sponzilli.g@gmail.com>
2024-12-07 14:06:13 +0000

    added code for summing account debits

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const fs = require("fs");
 // specify the path of the CSV file
 const path = "./lending-records.csv";
 
+let giadaAccount = 0;
+
+let claudiaAccount = 0;
+
+
 // Create a readstream
 // Parse options: delimiter and start from line 1
 
@@ -12,6 +17,21 @@ fs.createReadStream(path)
   .on("data", function (row) {
     // executed for each row of data
     console.log(row);
+    if (row[0] === "Giada") {
+      giadaAccount += Number(row[2])
+    } else if (row[0] === "Claudia") {
+      claudiaAccount += Number(row[2])
+    }
+    console.log(`Giada lent ${giadaAccount} to Claudia`)
+    console.log(`Claudia lent ${claudiaAccount} to Giada`)
+    if (giadaAccount > claudiaAccount) {
+      const difference = giadaAccount - claudiaAccount;
+      console.log(`Claudia is in debit of ${difference}`);
+    } else if (claudiaAccount > giadaAccount) {
+      const difference = claudiaAccount - giadaAccount;
+      console.log(`Giada is in debit of ${difference}`);
+    }
+
   })
   .on("error", function (error) {
     // Handle the errors
